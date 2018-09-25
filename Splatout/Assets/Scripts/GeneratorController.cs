395da8db7e7f17ecfdff6d971c83862643dcc970
peachpieproductions <benjames.im@gmail.com>
2018-09-24 21:33:09 -0400

    Update to unity version 2018.2.9f1, sphere prefab, sphere generator controller

---
+++ Splatout/Assets/Scripts/GeneratorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratorController : MonoBehaviour {
+
+    public GameObject spherePrefab;
+    public GameObject invertedSphere;
+    public int spawnAmount;
+
+    float invertedSphereRadius;
+    Transform sphereParent;
+
+    private void Start() {
+
+        invertedSphereRadius = invertedSphere.GetComponent<SphereCollider>().radius * invertedSphere.transform.localScale.x;
+
+        sphereParent = new GameObject("Sphere Parent").transform;
+
+        for (int i = 0; i < spawnAmount; i++) {
+            var newSphereInst = Instantiate(spherePrefab, sphereParent);
+            newSphereInst.transform.position = Random.insideUnitSphere * invertedSphereRadius * .5f;
+        }
+    }
+
+
+
+}
---
+++ Splatout/Assets/Scripts/InteractableSphere.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableSphere : MonoBehaviour {
+
+    Rigidbody rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate() {
+        //rb.vel
+    }
+
+
+
+
+}
