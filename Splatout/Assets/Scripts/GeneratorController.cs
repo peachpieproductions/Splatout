694b26315ceb72c24e6d8251479397c4db13511a
peachpieproductions <benjames.im@gmail.com>
2018-09-24 22:57:04 -0400

    Colored balls

--- Splatout/Assets/Scripts/GeneratorController.cs
+++ Splatout/Assets/Scripts/GeneratorController.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 public class GeneratorController : MonoBehaviour {
 
     public GameObject spherePrefab;
-    public GameObject invertedSphere;
+    public GameObject sphereContainer;
     public int spawnAmount;
+    public float spawnRadius;
 
-    float invertedSphereRadius;
     Transform sphereParent;
 
     private void Start() {
-
-        invertedSphereRadius = invertedSphere.GetComponent<SphereCollider>().radius * invertedSphere.transform.localScale.x;
 
         sphereParent = new GameObject("Sphere Parent").transform;
 
         for (int i = 0; i < spawnAmount; i++) {
             var newSphereInst = Instantiate(spherePrefab, sphereParent);
-            newSphereInst.transform.position = Random.insideUnitSphere * invertedSphereRadius * .5f;
+            newSphereInst.transform.position = Random.insideUnitSphere * spawnRadius;
         }
     }
 
--- Splatout/Assets/Scripts/InteractableSphere.cs
+++ Splatout/Assets/Scripts/InteractableSphere.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class InteractableSphere : MonoBehaviour {
 
+    public float minSpeed = 1;
+    public float maxSpeed = 5;
+    public float angularSpeed = 3;
+
     Rigidbody rb;
+    float speed;
+    Material mat;
 
     private void Awake() {
+        mat = GetComponent<MeshRenderer>().material;
+        transform.Rotate(Random.Range(0, 359), Random.Range(0, 359), Random.Range(0, 359));
+        speed = Random.Range(minSpeed, maxSpeed) * .5f; 
         rb = GetComponent<Rigidbody>();
+        StartCoroutine(ColorShift());
     }
 
     private void FixedUpdate() {
-        //rb.vel
+        
+        rb.velocity = transform.forward * speed;
+        if (transform.position.magnitude > 5) rb.velocity += -transform.position * .1f;
+        rb.angularVelocity += new Vector3(Random.Range(-angularSpeed, angularSpeed), 
+            Random.Range(-angularSpeed, angularSpeed), Random.Range(-angularSpeed, angularSpeed));
+    }
+
+    IEnumerator ColorShift() {
+
+        while (true) {
+
+            var col = mat.color;
+            Vector3 hsv;
+            Color.RGBToHSV(col, out hsv.x, out hsv.y, out hsv.z);
+            hsv.x = (transform.position.x + 20) / 30;
+            mat.color = Color.HSVToRGB(Mathf.Clamp01(hsv.x), .6f, 1);
+
+            yield return new WaitForSeconds(Random.Range(.4f, .5f));
+
+        }
+
     }
 
 
