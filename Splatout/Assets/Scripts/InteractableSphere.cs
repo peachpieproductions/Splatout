4f23aa706c996a72d3b98f141548d1cf7255d638
peachpieproductions <benjames.im@gmail.com>
2018-09-26 10:41:47 -0400

    Optimizations

--- Splatout/Assets/Scripts/InteractableSphere.cs
+++ Splatout/Assets/Scripts/InteractableSphere.cs
@@ -19,35 +19,26 @@ public class InteractableSphere : MonoBehaviour {
 
     Rigidbody rb;
     float speed;
-    Material mat;
 
     private void Awake() {
-        mat = GetComponent<MeshRenderer>().material;
+
         transform.Rotate(Random.Range(0, 359), Random.Range(0, 359), Random.Range(0, 359));
         speed = Random.Range(minSpeed, maxSpeed) * .5f; 
         rb = GetComponent<Rigidbody>();
-        //rb.velocity = transform.forward * speed;
 
-        //StartCoroutine(ColorShift());
         StartCoroutine(GenerateLines());
         StartCoroutine(UpdateLines());
-
-        /*int colorSamples = 7;
-        GradientColorKey[] colors = new GradientColorKey[colorSamples];
-        for(int i = 0; i < colorSamples; i++) {
-            colors[i].color = Color.HSVToRGB((1f / colorSamples) * i + .4f, 1, 1);
-            colors[i].time = (1f / colorSamples) * i + .05f;
-            Debug.Log(colors[i].time);
-        }
-        lineRendererGradientProfile.colorKeys = colors;*/
 
     }
 
     private void FixedUpdate() {
-        
+
         //Sphere Velocity
         rb.velocity += transform.forward * speed * Time.deltaTime * .25f;
-        if (transform.position.magnitude > 10f) rb.velocity += -transform.position * Time.deltaTime * .025f;
+        var horizontalMagnitude = new Vector3(transform.position.x, 0, transform.position.z * 1.5f).magnitude;
+        if (horizontalMagnitude > 10f) rb.velocity += -transform.position * Time.deltaTime * .025f;
+        var verticalMagnitude = new Vector3(0, transform.position.y, transform.position.z * 1.5f).magnitude;
+        if (verticalMagnitude > 6f) rb.velocity += -transform.position * Time.deltaTime * .04f;
         if (rb.velocity.magnitude > speed) rb.velocity = rb.velocity.normalized * speed;
         rb.angularVelocity += new Vector3(Random.Range(-angularSpeed, angularSpeed), 
             Random.Range(-angularSpeed, angularSpeed), Random.Range(-angularSpeed, angularSpeed));
@@ -70,7 +61,6 @@ public class InteractableSphere : MonoBehaviour {
                     GeneratorController.runtimeInst.lines.Remove(lines[i]);
                     lines.RemoveAt(i);
                 } else {
-                    //lines[i].line.material.color = mat.color;
                     lines[i].line.startColor = lineRendererGradientProfile.Evaluate((lines[i].line.GetPosition(0).x + 15) / 30f);
                     lines[i].line.endColor = lineRendererGradientProfile.Evaluate((lines[i].line.GetPosition(1).x + 15) / 30f);
                 }
@@ -88,32 +78,34 @@ public class InteractableSphere : MonoBehaviour {
 
         while (true) {
 
-            if (lines.Count < 1 && GeneratorController.runtimeInst.lines.Count < GeneratorController.runtimeInst.linesLimit) {
-
+            if (lines.Count < 3 && GeneratorController.runtimeInst.lines.Count < GeneratorController.runtimeInst.linesLimit) {
+                
                 var spheres = Physics.SphereCastAll(transform.position, 4f, Vector3.up, .01f);
 
                 foreach (RaycastHit hit in spheres) {
                     var sphere = hit.transform.GetComponent<InteractableSphere>();
                     if (sphere) {
-                        if (sphere == this) continue;
-                        if (sphere.lines.Count > 0 && sphere.lines[0].connectedSphere == this) continue;
-                        if (Random.value < .5f) {
+                        if (sphere == this || sphere.lines.Count > 2) continue;
+                        bool foundAttached = false;
+                        for (int i = 0; i < sphere.lines.Count; i++) { if (sphere.lines[i].connectedSphere == this) { foundAttached = true; break; } }
+                        if (foundAttached) continue;
+                        if (Random.value < .25f) {
                             LineConnection newLine = new LineConnection();
                             newLine.connectedSphere = sphere;
                             var lineRen = Instantiate(linePrefab);
                             newLine.line = lineRen;
                             lineRen.useWorldSpace = true;
-                            //lineRen.material.color = mat.color;
                             lineRen.SetPosition(0, transform.position);
                             lineRen.SetPosition(1, sphere.transform.position);
                             lineRen.startColor = lineRendererGradientProfile.Evaluate((lineRen.GetPosition(0).x + 15) / 30f);
                             lineRen.endColor = lineRendererGradientProfile.Evaluate((lineRen.GetPosition(1).x + 15) / 30f);
                             lines.Add(newLine);
                             GeneratorController.runtimeInst.lines.Add(newLine);
+                            if (lines.Count >= 3) break;
                         }
                     }
                 }
-
+                
             }
 
             yield return new WaitForSeconds(Random.Range(1f,3f));
@@ -122,22 +114,15 @@ public class InteractableSphere : MonoBehaviour {
 
     }
 
-    IEnumerator ColorShift() {
-        
-        while (true) {
-
-            var col = mat.color;
-            Vector3 hsv;
-            Color.RGBToHSV(col, out hsv.x, out hsv.y, out hsv.z);
-            hsv.x = (transform.position.x + 20) / 30;
-            mat.color = Color.HSVToRGB(Mathf.Clamp01(hsv.x), .6f, 1);
-
-            var xVel = Mathf.Abs(rb.velocity.x);
-            yield return new WaitForSeconds((6f - xVel) * .05f);
-
-        }
-        
+    //Code used to generate a special gradient (one-time use) - was used in Awake function
+    /*int colorSamples = 7;
+    GradientColorKey[] colors = new GradientColorKey[colorSamples];
+    for(int i = 0; i < colorSamples; i++) {
+        colors[i].color = Color.HSVToRGB((1f / colorSamples) * i + .4f, 1, 1);
+        colors[i].time = (1f / colorSamples) * i + .05f;
+        Debug.Log(colors[i].time);
     }
+    lineRendererGradientProfile.colorKeys = colors;*/
 
 
 
